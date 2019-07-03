29f003697201e12415b297be768beb6cc5ed80a3
Naoki Okubo <naoki.okubo.1108@icloud.com>
2019-07-03 19:52:28 +0900

    create: boards#showの生成・実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,25 +1,4 @@
 class ApplicationController < ActionController::Base
-  def index
-  end
-
-  def new
-  end
-
-  def create
-  end
-
-  def show
-  end
-
-  def edit
-  end
-
-  def update
-  end
-
-  def destroy
-  end
-
 
   # 以下、deviseのストロングパラメーター
   before_action :configure_permitted_parameters, if: :devise_controller?
