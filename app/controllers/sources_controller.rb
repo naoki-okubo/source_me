c5a058cfcdc4497742c5c0154fa3aeb49fa4080d
Naoki Okubo <naoki.okubo.1108@icloud.com>
2019-07-06 16:57:18 +0900

    create: sources#newの実装

--- app/controllers/sources_controller.rb
+++ app/controllers/sources_controller.rb
@@ -5,9 +5,15 @@ class SourcesController < ApplicationController
   end
 
   def new
+    @source  = Source.new
+    @board_id = params[:format]
+    @board = User.find(current_user.id).categories.where(board_id: "#{@board_id}")
   end
 
   def create
+    @params_source = params[:source]
+    @source  = Source.create(source_parems)
+    redirect_to source_path(@params_source[:board_id])
   end
 
   def show
@@ -25,5 +31,11 @@ class SourcesController < ApplicationController
 
   def set_user
     @user = User.find(current_user.id)
+  end
+
+  private
+
+  def source_parems
+    params.require(:source).permit(:url, :title, :text, :board_id, :category_id).merge(user_id: current_user.id)
   end
 end
