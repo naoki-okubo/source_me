a83d2e3b7f43b3c43c6a7a6d34338b891a42663e
Naoki Okubo <naoki.okubo.1108@icloud.com>
2019-07-17 13:34:58 +0900

    fix:ボード一覧ページ プロフィールページの非同期表示の実装

--- app/assets/javascripts/board/show.js
+++ app/assets/javascripts/board/show.js
@@ -10,9 +10,7 @@ $(document).on('turbolinks:load',function(e){
     var close = 
                 `
                   <div class="header">
-                    <a href="/users/${user.id}">
-                      <i class="fas fa-user-circle"></i>
-                    </a>
+                    <i class="fas fa-user-circle"></i>
                     <a rel="nofollow" data-method="delete" href="/users/sign_out">
                       <i class="fas fa-sign-out-alt"></i>
                     </a>
@@ -70,15 +68,105 @@ $(document).on('turbolinks:load',function(e){
                       </div>
                     </form></div>
                   `
+    $('.boards-new').remove();
+    $('.fa-plus-circle').css("animation","none");
     $(newBoard).appendTo('.board-show').hide().fadeIn(300);
-
     $(document).on("click", ".back-btn", function (e) {
       e.preventDefault();
+      $('.fa-plus-circle').css("animation","top-jump_effect 1.5s infinite ease 0s");
       $('.boards-new').remove();
     });
   });
 
 
+
+
+  $(document).on("click",'.fa-user-circle', function (e) {
+    e.preventDefault();
+    console.log(gon.authenticity_token);
+  
+    var profile = 
+                  `
+                    <div class="users-edit">
+                      <div class="new-main">
+                        <a class="back-btn">
+                          <div class="submit-back">
+                            <i class="far fa-times-circle"></i>
+                          </div>
+                        </a>
+                        <form class="edit_user" id="edit_user" action="/users" accept-charset="UTF-8" method="post"><input name="utf8" type="hidden" value="✓"><input type="hidden" name="_method" value="put"><input type="hidden" name="authenticity_token" value="${gon.authenticity_token}"><div class="sourceCreate">
+                        <div class="sourceCreate__title">
+                          <p>Profile</p>
+                        </div>
+                        <div class="sourceCreate__form">
+                          <div class="form-contents">
+                            <div class="form">
+                              <p>ニックネーム　Nick name</p>
+                              <input autofocus="autofocus" class="value" type="text" value="${gon.user.nickname}" name="user[nickname]" id="user_nickname">
+                            </div>
+                            <div class="form">
+                              <p>メールアドレス　E-mail</p>
+                              <input autocomplete="email" class="value" type="email" value="${gon.user.email}" name="user[email]" id="user_email">
+                            </div>
+                            <div class="form">
+                              <p>姓　Family-name</p>
+                              <input class="value" type="text" value="${gon.user.family_name}" name="user[family_name]" id="user_family_name">
+                            </div>
+                            <div class="form">
+                              <p>名　First-name</p>
+                              <input class="value" type="text" value="${gon.user.first_name}" name="user[first_name]" id="user_first_name">
+                            </div>
+                            <div class="form">
+                              <p>姓(カナ)　Family-name(kana)</p>
+                              <input class="value" type="text" value="${gon.user.family_name_kana}" name="user[family_name_kana]" id="user_family_name_kana">
+                            </div>
+                            <div class="form">
+                              <p>名(カナ)　First-name(kana)</p>
+                              <input class="value" type="text" value="${gon.user.first_name_kana}" name="user[first_name_kana]" id="user_first_name_kana">
+                            </div>
+                            <div class="form">
+                              <p class="any">パスワードを変更する</p>
+                              <p class="any">Edit-Password</p>
+                              <input autocomplete="new-password" placeholder="変更しない場合は空白（ ６文字以上 ）" class="value" type="password" name="user[password]" id="user_password">
+                            </div>
+                            <div class="form">
+                              <p class="any">パスワードを変更する(確認)</p>
+                              <p class="any">Edit-Password(Confirmation)</p>
+                              <input autocomplete="new-password" placeholder="変更しない場合は空白" class="value" type="password" name="user[password_confirmation]" id="user_password_confirmation">
+                            </div>
+                            <div class="form form_current-password">
+                              <p>現在のパスワード</p>
+                              <p>Current-Password</p>
+                              <input autocomplete="current-password" placeholder="６文字以上" class="value" type="password" name="user[current_password]" id="user_current_password">
+                            </div>
+                          </div>
+                          <div class="submit">
+                            <input type="submit" name="commit" value="更新" class="submit__btn">
+                          </div>
+                          </div>
+                        </div>
+                      </form>
+                      <div class="submit delete-account">
+                      <p>
+                        アカウント情報の削除
+                      </p>
+                      <form class="button_to" method="post" action="/users">
+                        <input type="hidden" name="_method" value="delete">
+                        <input data-confirm="本当に退会しますか？退会手続きを行うと今まで保存した内容の全てが失われます。" class="cancel-btn" type="submit" value="退会する">
+                        <input type="hidden" name="authenticity_token" value="${gon.authenticity_token}">
+                      </form>
+                    </div>
+                  </div>
+                `
+    $('.users-edit').remove();
+    $(profile).appendTo('.board-show').hide().fadeIn(300);
+    $('.fa-plus-circle').css("animation","none");
+    $(document).on("click", ".back-btn", function (e) {
+      e.preventDefault();
+      $('.fa-plus-circle').css("animation","top-jump_effect 1.5s infinite ease 0s");
+      $('.users-edit').remove();
+    });
+  });
 
 
   $(document).on("mouseenter",'.fa-plus-circle', function (e) {
